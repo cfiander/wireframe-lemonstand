5d115f5c0043680ec97d47883142b211149b12f1
cfiander <cfiander90@gmail.com>
2019-06-17 01:59:17 -0700

    Updates

---
+++ precache-manifest.a90db70aad64cb8332408dcb853686d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec37d18322b39feac6e095c40034f922",
+    "url": "/wireframe-lemonstand/index.html"
+  },
+  {
+    "revision": "4bf046c4f5839dfcb800",
+    "url": "/wireframe-lemonstand/static/css/main.1c391ab9.chunk.css"
+  },
+  {
+    "revision": "6e5b05b7cc7a0875f6af",
+    "url": "/wireframe-lemonstand/static/js/2.1ce572ce.chunk.js"
+  },
+  {
+    "revision": "4bf046c4f5839dfcb800",
+    "url": "/wireframe-lemonstand/static/js/main.8dc9a555.chunk.js"
+  },
+  {
+    "revision": "c4f0431fd44ef0f74f94",
+    "url": "/wireframe-lemonstand/static/js/runtime~main.cdae6e65.js"
+  }
+]);
